725981ba130a511a8d87d6bb8ef3a56404e512fe
Gennady Dobretsov <dobrets@gmail.com>
2022-09-25 20:56:53 +0600

    updateColor func add

--- swiftbook_2_4/ViewController.swift
+++ swiftbook_2_4/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    
     @IBOutlet var redSlider: UISlider!
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
@@ -19,14 +21,37 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    
         redSliderLabel.text = String(format: "%.2f", redSlider.value)
         greenSliderLabel.text = String(format: "%.2f", greenSlider.value)
         blueSliderLabel.text = String(format: "%.2f", blueSlider.value)
+        
+        colorView.layer.cornerRadius = 10
+        updateColor()
     }
 
     @IBAction func redSliderChanged() {
         redSliderLabel.text = String(format: "%.2f", redSlider.value)
+        updateColor()
     }
     
+    @IBAction func greenSliderChanged() {
+        greenSliderLabel.text = String(format: "%.2f", greenSlider.value)
+        updateColor()
+    }
+    
+    @IBAction func blueSliderChanged() {
+        blueSliderLabel.text = String(format: "%.2f", blueSlider.value)
+        updateColor()
+    }
+    
+    private func updateColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
 }
 
