dc13ebc96eeab7b7260cb474febeee1e1ab86e29
Gennady Dobretsov <dobrets@gmail.com>
2022-10-12 21:32:38 +0600

    pass color through delegate

--- swiftbook_2_4/MainViewController.swift
+++ swiftbook_2_4/MainViewController.swift
@@ -7,28 +7,26 @@
 
 import UIKit
 
+protocol SettingsViewControllerDelegate {
+    func setNewBackground(for color: UIColor)
+}
+
 class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let settingsVC = segue.destination as? SettingsViewController else { return }
         settingsVC.color = view.backgroundColor ?? UIColor.white
+        settingsVC.delegate = self
     }
     
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension MainViewController: SettingsViewControllerDelegate {
+    func setNewBackground(for color: UIColor) {
+        view.backgroundColor = color
     }
-    */
-
 }
--- swiftbook_2_4/SettingsViewController.swift
+++ swiftbook_2_4/SettingsViewController.swift
@@ -25,6 +25,7 @@ class SettingsViewController: UIViewController {
     @IBOutlet var blueSliderTF: UITextField!
     
     var color: UIColor!
+    var delegate: SettingsViewControllerDelegate!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -60,6 +61,17 @@ class SettingsViewController: UIViewController {
         }
         
         updateColor()
+    }
+    
+    @IBAction func doneButtonTapped() {
+        color = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+        delegate.setNewBackground(for: color)
+        dismiss(animated: true)
     }
     
     // MARK: Private functions
