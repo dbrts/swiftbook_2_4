25ab066bb06a3e2a1feb9f93de51f15f6c067359
Gennady Dobretsov <dobrets@gmail.com>
2022-10-12 22:42:15 +0600

    deleted empty viewDidLoad

--- swiftbook_2_4/MainViewController.swift
+++ swiftbook_2_4/MainViewController.swift
@@ -12,10 +12,6 @@ protocol SettingsViewControllerDelegate {
 }
 
 class MainViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let settingsVC = segue.destination as? SettingsViewController else { return }
